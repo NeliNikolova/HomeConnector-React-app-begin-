d4587007cbc8fa442ed3213607cacae35e046e38
Nelly Nikolova <32382624+NeliNikolova@users.noreply.github.com>
2018-08-22 14:46:20 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://neli:neliradkova1982@ds129541.mlab.com:29541/home",
+  mongoURI: "mongodb://****:*****@*****.mlab.com:******/home",
   secretOrKey: "secret"
 };
